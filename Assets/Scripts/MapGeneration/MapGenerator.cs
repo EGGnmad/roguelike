b84e4f1b90c5ddd6fb4b2d68e100242779666f13
EGGnmad <viewnono1219@gmail.com>
2024-04-06 22:29:46 +0900

    Make Generate() more efficient

--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -12,19 +12,17 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
 {
     #region Fields:Serialized
 
-    [Header("Generation:Config")]
-    public float boostTimeScale = 50;
+    [Header("Generation:Config")] public float boostTimeScale = 50;
     [SerializeField] private RoomBehavior roomPrefab;
-    
-    [Header("Generation:Room")]
-    public float radius;
+
+    [Header("Generation:Room")] public float radius;
     public int minRoomSize;
     public int maxRoomSize;
     public int maxRoomCount;
 
-    [Header("Generation:MST")] 
-    [Range(0,1)] public float randomPathValue = 0.1f;
-    
+    [Header("Generation:MST")] [Range(0, 1)]
+    public float randomPathValue = 0.1f;
+
     #endregion
 
     #region Fields:private
@@ -35,14 +33,14 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
     private Edge[] _minimumEdges;
 
     #endregion
-    
+
     #region Methods:Interface
 
     public IRoom[] GetRooms()
     {
         return _rooms;
     }
-    
+
     public Triangulation GetTriangulation()
     {
         return _triangulation;
@@ -59,7 +57,7 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
 
     private void Start()
     {
-        Generate();
+        UniTask.Create(Generate);
     }
 
     #endregion
@@ -73,23 +71,23 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
         newRoom.transform.localPosition = pos;
         newRoom.transform.localScale = size;
         newRoom.Index = index;
-        
+
         return newRoom;
     }
 
     private RoomBehavior[] GenerateRooms(int roomCount)
     {
         var rooms = new RoomBehavior[roomCount];
-        
+
         for (int i = 0; i < roomCount; i++)
         {
             Vector2 randomPos = Random.insideUnitCircle * radius;
-            Vector2 size = new Vector2(Random.value, Random.value) * (maxRoomSize-minRoomSize)/2f;
-            size.x = Mathf.Round(size.x + minRoomSize/2f);
-            size.y = Mathf.Round(size.y + minRoomSize/2f);
+            Vector2 size = new Vector2(Random.value, Random.value) * (maxRoomSize - minRoomSize) / 2f;
+            size.x = Mathf.Round(size.x + minRoomSize / 2f);
+            size.y = Mathf.Round(size.y + minRoomSize / 2f);
             size.x *= 2f;
             size.y *= 2f;
-            
+
             rooms[i] = CreateRoom(randomPos, size, i);
         }
 
@@ -110,7 +108,7 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
         // averageHeight /= allRooms.Length;
         //
         // return allRooms.Where(room => room.transform.localScale.x >= 1.25f * averageWidth & room.transform.localScale.y >= 1.25f * averageHeight).ToArray();
-        
+
         // area
         float averageArea = 0f;
         foreach (var room in allRooms)
@@ -120,7 +118,28 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
 
         averageArea /= allRooms.Length;
 
-        return allRooms.Where(room => room.transform.localScale.x * room.transform.localScale.y >= 1.5f * averageArea).ToArray();
+        return allRooms.Where(room => room.transform.localScale.x * room.transform.localScale.y >= 1.5f * averageArea)
+            .ToArray();
+    }
+
+    private async UniTask SeperationTask(RoomBehavior[] rooms)
+    {
+        await UniTask.SwitchToThreadPool();
+
+        bool isRoomsStop = false;
+
+        while (!isRoomsStop)
+        {
+            isRoomsStop = true;
+            foreach (var room in rooms)
+            {
+                isRoomsStop = isRoomsStop && room.Velocity == Vector3.zero && room.isSeperationStart;
+            }
+
+            await UniTask.Delay(TimeSpan.FromSeconds(0.1f));
+        }
+
+        await UniTask.SwitchToMainThread();
     }
 
     private Triangulation GetDelaunayTriangle(RoomBehavior[] mainRooms)
@@ -130,44 +149,25 @@ public class MapGenerator : MonoBehaviour, IMapGenerator, IDelaunayTriangulation
         return triangulation;
     }
     
-    private async UniTask SeperationTask(RoomBehavior[] rooms)
-    {
-        await Task.Run(async () =>
-        {
-            bool isRoomsStop = false;
-
-            while (!isRoomsStop)
-            {
-                
-                isRoomsStop = true;
-                foreach (var room in rooms)
-                {
-                    isRoomsStop = isRoomsStop && room.Velocity == Vector3.zero && room.isSeperationStart;
-                }
-                
-                await Task.Delay(TimeSpan.FromSeconds(0.1f));
-            }
-        });
-    }
-    
     public async UniTask Generate()
     {
+        // boost map generation speed
         Time.timeScale = boostTimeScale;
-        
+
         // generate rooms
         _rooms = GenerateRooms(maxRoomCount);
         _mainRooms = GetMainRooms(_rooms);
-        
+
         // wait until SeperationTask end
         await SeperationTask(_rooms);
-        
+
         // get delaunay triangles
         _triangulation = GetDelaunayTriangle(_mainRooms);
-        
+
         // get MST
         _minimumEdges = new MinimumSpanningTree(_triangulation, randomPathValue).GetSpanningTree();
-        
-        
+
+        // back to normal time scale
         Time.timeScale = 1f;
     }
 
